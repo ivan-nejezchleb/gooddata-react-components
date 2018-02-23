26e2df9381b1ab5df10d772bbc0d3d11c700b10d
vaclavbohac <bohac.v@gmail.com>
2018-02-23 16:43:20 +0100

    RELATED: RAIL-655 Add components with bucket interface

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { VisualizationObject, AFM } from '@gooddata/typings';
+
+import { BarChart as CoreBarChart } from './core/BarChart';
+import { ICommonChartProps } from './core/base/BaseChart';
+import { dataSourceProvider } from './afm/DataSourceProvider';
+import { convertBucketsToAFM } from '../helpers/conversion';
+import { generateStackedDimensions } from '../helpers/dimensions';
+import { isStackedChart } from '../helpers/stacks';
+
+export interface IBarChartProps extends ICommonChartProps {
+    projectId: string;
+    measures: VisualizationObject.BucketItem[];
+    attributes?: VisualizationObject.IVisualizationAttribute[];
+    stacks?: VisualizationObject.IVisualizationAttribute[];
+    filters?: VisualizationObject.VisualizationObjectFilter[];
+}
+
+function generateDefaultDimensions(afm: AFM.IAfm): AFM.IDimension[] {
+    return [
+        {
+            itemIdentifiers: ['measureGroup']
+        },
+        {
+            itemIdentifiers: (afm.attributes || []).map(a => a.localIdentifier)
+        }
+    ];
+}
+
+function getStackingResultSpec(buckets: VisualizationObject.IBucket[]): AFM.IResultSpec {
+    if (isStackedChart(buckets)) {
+        return {
+            dimensions: generateStackedDimensions(buckets)
+        };
+    }
+
+    return {
+        dimensions: generateDefaultDimensions(convertBucketsToAFM(buckets))
+    };
+}
+
+export function BarChart(props: IBarChartProps): JSX.Element {
+    const Component = dataSourceProvider(CoreBarChart, generateDefaultDimensions);
+
+    const buckets: VisualizationObject.IBucket[] = [
+        {
+            localIdentifier: 'measures',
+            items: props.measures || []
+        },
+        {
+            localIdentifier: 'attributes',
+            items: props.attributes || []
+        },
+        {
+            localIdentifier: 'stacks',
+            items: props.stacks || []
+        }
+    ];
+
+    return (
+        <Component
+            {...props}
+            projectId={props.projectId}
+            afm={convertBucketsToAFM(buckets, props.filters)}
+            resultSpec={getStackingResultSpec(buckets)}
+        />
+    );
+}
