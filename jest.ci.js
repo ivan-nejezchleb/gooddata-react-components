92b791d5dbd9445c9642b71ee0cf88b6de6e50d6
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2019-12-09 08:58:31 +0100

    * remove logs

--- jest.ci.js
+++ jest.ci.js
@@ -1,10 +1,10 @@
 // (C) 2019 GoodData Corporation
 console.log(process.env.NODE_ICU_DATA);
 
-process.env = Object.assign(process.env, { NODE_ICU_DATA: 'node_modules/full-icu' });
-const icu = require('full-icu');
+// process.env = Object.assign(process.env, { NODE_ICU_DATA: 'node_modules/full-icu' });
+// const icu = require('full-icu');
 
-console.log(icu.nodever, icu.icudat, icu.noi18n);
+// console.log(icu.nodever, icu.icudat, icu.noi18n);
 
 const jestConfig = require('./jest.config.js');
 
